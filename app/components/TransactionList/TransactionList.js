2ea65580258830ad824132aed9cb691dcbb636dd
Arthur Vincent Simon <developarvin@gmail.com>
2017-03-05 16:57:15 +0800

    Create initial component

---
+++ app/components/TransactionList/TransactionList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    ListView
+} from 'react-native';
+
+class TransactionList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>This is the Transaction List component</Text>
+            </View>
+        )
+    }
+}
+
+export default TransactionList;
---
+++ app/components/TransactionList/index.js
@@ -0,0 +1,3 @@
+import TransactionList from './TransactionList.js';
+
+export default TransactionList;
