e586b3f20836be7289f9626291561afbe0950842
Arthur Vincent Simon <developarvin@gmail.com>
2017-03-05 17:12:54 +0800

    Create skeleton of TransactionList component

--- app/components/TransactionList/TransactionList.js
+++ app/components/TransactionList/TransactionList.js
@@ -6,13 +6,46 @@ import {
 } from 'react-native';
 
 class TransactionList extends Component {
-    render() {
+    _renderRow() {
         return (
             <View>
-                <Text>This is the Transaction List component</Text>
+                <View>
+                    <Text>Title</Text>
+                </View>
+                <View>
+                    <Text>Description</Text>
+                </View>
+                <View>
+                    <Text>Category</Text>
+                </View>
+                <View>
+                    <Text>Outflow</Text>
+                </View>
+                <View>
+                    <Text>Inflow</Text>
+                </View>
             </View>
         )
     }
+
+    render() {
+        // Boilerplate for adding data to the vie 
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        // Data is gotten from `stories` props
+        const dataSource = ds.cloneWithRows(this.props.stories);
+
+        return (
+            <ListView 
+                dataSource={dataSource}
+                enableEmptySections={true}
+                renderRow={this._renderRow.bind(this)}
+            />
+        )
+    }
+}
+
+TransactionList.defaultProps = {
+    stories: [1,2,3,4,5]
 }
 
 export default TransactionList;
