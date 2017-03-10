62ddfcf107c333b3b2534c866e9ec2ae8c80d1ab
Arthur Vincent Simon <developarvin@gmail.com>
2017-03-10 18:29:57 +0800

    Add navigation to add and update screen navigation

--- app/screens/HomeScreen/HomeScreen.js
+++ app/screens/HomeScreen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 class HomeScreen extends Component {
     static navigationOptions = {
@@ -7,9 +7,17 @@ class HomeScreen extends Component {
     };
 
     render() {
+        const { navigate } = this.props.navigation;
+
         return (
             <View>
                 <Text>This is the Home Screen</Text>
+                <Button 
+                    onPress={() => navigate('AddTransaction')}
+                    title="Go to Add Transaction Screen" />
+                <Button
+                    onPress={() => navigate('UpdateTransaction')}
+                    title="Go to Update Transaction Screen" />
             </View>
         );
     }
