79b90fe070bdb9d91d7c50f60d722172d4fef588
Arthur Vincent Simon <developarvin@gmail.com>
2017-04-20 17:49:17 +0800

    Refactor out App Navigator to its own container

---
+++ src/containers/AppWithNavigationState.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { StackNavigator, addNavigationHelpers } from 'react-navigation'
+import { connect } from 'react-redux'
+
+// Configuration variables
+import { AppRouteConfigs } from '../config/navigation'
+import Config from 'react-native-config'
+
+const AppNavigator = StackNavigator(AppRouteConfigs)
+
+class AppWithNavigationState extends Component {
+  render() {
+    const { dispatch, nav } = this.props
+    return (
+      <AppNavigator navigation={
+        addNavigationHelpers({
+          dispatch: dispatch,
+          state: nav
+        })} 
+      />
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    nav: state.nav
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => { 
+  return {
+    dispatch: dispatch
+  } 
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppWithNavigationState)
--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { StackNavigator, addNavigationHelpers } from 'react-navigation'
 import { fetchFirebaseTransactions } from './store/data'
-
-// Configuration variables
-import { AppRouteConfigs } from './config/navigation'
-import Config from 'react-native-config'
 
 // Store
 import store from './store'
 
+// App with Navigation State
+import AppWithNavigationState from './containers/AppWithNavigationState'
+
 // Initial fetch of firebase data
 store.dispatch(fetchFirebaseTransactions())
-
-const AppNavigator = StackNavigator(AppRouteConfigs)
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <AppNavigator />
+        <AppWithNavigationState />
       </Provider>
 
     )
